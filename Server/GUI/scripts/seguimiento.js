fca93f32783a9d9424ba868968f317f3407648ec
LauAubert <aubertlautaroa@gmail.com>
2021-10-02 12:50:45 -0300

    feat: add status bar function

---
+++ Server/GUI/scripts/seguimiento.js
@@ -0,0 +1,47 @@
+const barra = document.querySelector('.steps')
+// var estado = 0;
+
+function actualizar(estado){
+    const items = barra.querySelectorAll('.step-item')
+    const etiquetas = ['is-active','is-completed','is-success','is-danger']
+    console.log(items)
+    for(let i=0;i<items.length;i++){
+        for(let j=0;j<etiquetas.length;j++){
+            items[i].classList.remove(etiquetas[j])
+        }
+    }
+
+    if (estado == 0){
+        items[0].classList.add('is-active')
+    }
+    else if (estado == 1){
+        items[0].classList.add('is-completed')
+        items[0].classList.add('is-success')
+        items[1].classList.add('is-active')
+    }
+    else if (estado == 2){
+        items[0].classList.add('is-completed')
+        items[0].classList.add('is-success')
+        items[1].classList.add('is-completed')
+        items[1].classList.add('is-success')
+        items[2].classList.add('is-active')
+    }
+    else if (estado == 3){
+        items[0].classList.add('is-completed')
+        items[0].classList.add('is-success')
+        items[1].classList.add('is-completed')
+        items[1].classList.add('is-success')
+        items[2].classList.add('is-completed')
+        items[2].classList.add('is-success')
+        items[3].classList.add('is-completed')
+        items[3].classList.add('is-success')
+        document.getElementById('Devolbutton').disabled = false;
+    }
+    else if (estado ==4){
+        items[0].classList.add('is-completed')
+        items[0].classList.add('is-danger')
+        items[0].querySelector('i.fa').classList.remove('fa-check')
+        items[0].querySelector('i.fa').classList.add('fa-times')
+    } 
+}
+
