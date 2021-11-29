fa2979b7df979714a1ca9ba9976e21dceb9a411e
luciofuster <luciofuster@gmail.com>
2021-11-29 16:33:02 -0300

    Fix: Nombre de red a conectar

--- esp32-vehículo/boot.py
+++ esp32-vehículo/boot.py
@@ -7,6 +7,6 @@ import time
 sta_if = network.WLAN(network.STA_IF);    # instancia el objeto -sta_if- para controlar la interfaz STA
 sta_if.active(True)                       # activa la interfaz STA del ESP32
 if sta_if.isconnected(): sta_if.disconnect()
-sta_if.connect("Avionica-2", "Atlantida2020")    # inicia la conexión con el AP, "nombre - contraseña" de red
+sta_if.connect("Avionica 2B", "Atlantida2020")    # inicia la conexión con el AP, "nombre - contraseña" de red
 while not sta_if.isconnected():
     time.sleep(0.5)
